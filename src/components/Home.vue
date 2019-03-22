9b287fa720f9ab37c0615d38066885495626274b
Al <al.cuthbert@yandex.ru>
2019-03-22 16:36:49 +0300

    Home component: jumbotron

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div>
+    <b-jumbotron>
+      <template slot="header">Estimation Tool</template>
+
+      <template slot="lead">
+        This is a simple application for the load estimation of tasks
+      </template>
+
+      <hr class="my-4" />
+
+      <p>
+        To start working you can go to:
+      </p>
+
+      <b-button variant="primary" href="#" :to="{name: $routeNames.changeRequests}">Change Requests page</b-button>
+    </b-jumbotron>
   </div>
 </template>
