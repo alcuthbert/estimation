8e04edc936f586f776b5ec91cbf53bc969a8e664
Al <al.cuthbert@yandex.ru>
2019-04-04 09:23:55 +0300

    ChangeRequest service: new methods added

--- src/common/services/ChangeRequests.js
+++ src/common/services/ChangeRequests.js
@@ -1,8 +1,23 @@
 import axios from 'axios';
+import {API_URL} from './../../appConfig.js'
+
+const url = `${API_URL}/change-requests`;
 
 export default {
-    getItems() {
-        return axios.get("/api/change-requests")
-            .then((response) => response.data.data)
+  getApiUrl() {
+    return url;
+  },
+  getItems() {
+    return axios.get(url);
+  },
+  save(obj) {
+    if (Object.keys(obj).includes('id') && obj.id) {
+      return axios.put(`${url}/${obj.id}`, obj);
+    } else {
+      return axios.post(url, obj);
     }
+  },
+  delete(id) {
+    return axios.delete(`${url}/${id}`);
+  }
 }
