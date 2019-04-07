621f310ada149a926572b0627249039b29e62390
Savitskiy <aleksey.savitskiy@t-systems.ru>
2019-04-07 09:55:49 +0300

    vee validate ussage

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import VeeValidate from 'vee-validate';
 
 import App from './App.vue'
 import RouteNames from './routeNames.js'
@@ -14,6 +15,11 @@ import './configs/filters.js'
 
 Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
+Vue.use(VeeValidate, {
+	inject: true,
+	// Important to name this something other than 'fields'
+	fieldsBagName: 'veeFields'
+})
 
 Vue.prototype.$routeNames = RouteNames;
 
@@ -22,8 +28,8 @@ import './styles/styles.scss'
 Vue.config.productionTip = false
 
 new Vue({
-  store,
-  router,
-  i18n,
-  render: h => h(App),
+	store,
+	router,
+	i18n,
+	render: h => h(App),
 }).$mount('#app')
