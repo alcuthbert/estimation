537eab16ef45edcb390f677775b408fe5cd9618f
Al <al.cuthbert@yandex.ru>
2019-04-11 16:33:02 +0300

    resources/statuses.js created

---
+++ src/resources/statuses.js
@@ -0,0 +1,6 @@
+export const STATUS_WAITING_FOR_APPROVE = 1
+export const STATUS_APPROVED = 2
+export const STATUS_ASSIGNED = 3
+export const STATUS_WAITING_FOR_MERGE = 4
+export const STATUS_MERGED = 5
+export const STATUS_CLOSED = 6
