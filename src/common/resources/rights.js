510148c57e5a503cfa555da7b302a2162ae8b363
Al <al.cuthbert@yandex.ru>
2019-04-16 17:41:11 +0300

    commmon/resources/rights created

---
+++ src/common/resources/rights.js
@@ -0,0 +1,5 @@
+export const RIGHTS_CR_CREATE = 'cr::create'
+export const RIGHTS_CR_EDIT = 'cr::edit'
+export const RIGHTS_CR_DELETE = 'cr::delete'
+export const RIGHTS_CR_APPROVE = 'cr::approve'
+export const RIGHTS_CR_ASSIGN = 'cr::assign'
