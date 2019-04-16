63a2e59f12c7eeca25e2224d5e7c103bbb78e1a8
Al <al.cuthbert@yandex.ru>
2019-04-16 17:41:42 +0300

    RightsSevice::check() method created

--- src/common/services/Rights.js
+++ src/common/services/Rights.js
@@ -2,12 +2,33 @@ import {ROLE_ADMIN} from '@/resources/roles'
 import {ROLE_ARCHITECT} from '@/resources/roles'
 import {ROLE_MANAGER} from '@/resources/roles'
 
-export const RIGHTS_CR_EDIT = 'cr::edit'
-export const RIGHTS_CR_DELETE = 'cr::delete'
-export const RIGHTS_CR_APPROVE = 'cr::approve'
-export const RIGHTS_CR_ASSIGN = 'cr::assign'
+import {RIGHTS_CR_CREATE} from '@/common/resources/rights.js'
+import {RIGHTS_CR_EDIT} from '@/common/resources/rights.js'
+import {RIGHTS_CR_DELETE} from '@/common/resources/rights.js'
+import {RIGHTS_CR_APPROVE} from '@/common/resources/rights.js'
+import {RIGHTS_CR_ASSIGN} from '@/common/resources/rights.js'
 
 export default {
+    check(role, right) {
+        if (role === null || role === undefined) {
+            return false
+        }
+
+        switch(right) {
+            case RIGHTS_CR_CREATE:
+                return this.changeRequestCreate(role)
+            case RIGHTS_CR_EDIT:
+                return this.changeRequestEdit(role)
+            case RIGHTS_CR_DELETE:
+                return this.changeRequestDelete(role)
+            case RIGHTS_CR_APPROVE:
+                return this.changeRequestApprove(role)
+            case RIGHTS_CR_ASSIGN:
+                return this.changeRequestAssign(role)
+            default:
+                return false
+        }
+    },
     changeRequestCreate(role) {
         return (role === ROLE_ADMIN || role === ROLE_ARCHITECT) ? true : false
     },
