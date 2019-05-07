63bdbef4c560cfbc92da4b89fd0a75c8a2f20474
al cuthbert <al.cuthbert@yandex.ru>
2019-05-07 11:25:14 +0300

    Profile: removed unused things

--- src/components/pages/Profile.vue
+++ src/components/pages/Profile.vue
@@ -1,21 +1,27 @@
 <template>
-	<div class="row m-3">
-		<div class="col-sm-4 col-md-4 col-lg-4 mx-auto">
-			<b-card no-body v-if="user">
-				<h4 slot="header">My Profile</h4>
-
-				<b-card-body>
-					<b-card-title>{{ '#' + user.id + ': ' + user.name }} </b-card-title>
-					<b-card-sub-title class="mb-2">Nick: {{user.nickname}}</b-card-sub-title>
-					<b-card-text>
-						Some quick example text to build on the card title and make up the bulk of the card's
-						content.
-					</b-card-text>
-				</b-card-body>
+	<b-row>
+		<b-col sm=12 md=12 lg=12 mx="auto">
+			<b-card no-body v-if="identity">
+				<h4 slot="header">
+					My Profile
+				</h4>
 
 				<b-list-group flush>
-					<b-list-group-item>Sex: {{user.gender}}</b-list-group-item>
-					<b-list-group-item>Birth date: {{user.birthdate}}</b-list-group-item>
+					<b-list-group-item>
+						Name: {{identity.name}}
+					</b-list-group-item>
+
+					<b-list-group-item>
+						Role: {{identity.role}}
+					</b-list-group-item>
+
+					<b-list-group-item>
+						Email: {{identity.email}}
+					</b-list-group-item>
+
+					<b-list-group-item>
+						Created at: {{identity.created_at}}
+					</b-list-group-item>
 				</b-list-group>
 			</b-card>
 
@@ -28,43 +34,31 @@
 					</b-card-text>
 				</b-card-body>
 			</b-card>
-		</div>
-
-		<div class="col-sm-8 col-md-8 col-lg-8 mx-auto">
-			<b-card no-body v-if="user">
-				<h4 slot="header">My Tasks</h4>
-
-				<b-card-body>
-					<tasks :cr-id="crId"></tasks>
-				</b-card-body>
-			</b-card>
-		</div>
-	</div>
+		</b-col>
+	</b-row>
 </template>
 
 <script>
-import axios from 'axios'
-import Tasks from "@/components/tasks/Tasks";
+import { mapGetters } from "vuex";
+import { GET_IDENTITY } from '@/store/getter-types'
+// import Task from "@/components/tasks/Task";
 
 export default {
 	data() {
 		return {
-			userId: 1,
-			crId: 2,
-			user: null
+			
 		}
 	},
+	computed: {
+		...mapGetters({
+			identity: GET_IDENTITY
+		})
+	},
 	beforeMount() {
-		axios
-			.get('/api/users')
-			.then(response => (
-				this.user = response.data.data.filter( user => {
-					return user.id == this.userId
-				})[0]
-			))
+		
 	},
 	components: {
-		Tasks
+		// Task
 	}
 }
 </script>
