6e046c398fe5255d28108565be86fd5bc659e60e
Savitskiy <aleksey.savitskiy@t-systems.ru>
2019-05-10 10:55:59 +0300

    rights code refactored

--- src/common/services/Rights.js
+++ src/common/services/Rights.js
@@ -1,25 +1,47 @@
-import {ROLE_ADMIN} from '@/common/resources/roles'
-import {ROLE_ARCHITECT} from '@/common/resources/roles'
-import {ROLE_MANAGER} from '@/common/resources/roles'
-import {ROLE_ESTIMATOR} from '@/common/resources/roles'
+import {
+    RIGHTS_CR_CREATE,
+    RIGHTS_CR_EDIT,
+    RIGHTS_CR_DELETE,
+    RIGHTS_CR_APPROVE,
+    RIGHTS_CR_ASSIGN,
+    RIGHTS_CR_CLOSE,
+    RIGHTS_CR_MERGE,
+    RIGHTS_TASK_CREATE,
+    RIGHTS_TASK_EDIT,
+    RIGHTS_TASK_DELETE,
+    RIGHTS_SUBTASK_CREATE,
+    RIGHTS_SUBTASK_EDIT,
+    RIGHTS_SUBTASK_DELETE,
+    RIGHTS_ESTIMATION_CREATE
+} from '@/common/resources/rights'
 
-import {RIGHTS_CR_CREATE} from '@/common/resources/rights.js'
-import {RIGHTS_CR_EDIT} from '@/common/resources/rights.js'
-import {RIGHTS_CR_DELETE} from '@/common/resources/rights.js'
-import {RIGHTS_CR_APPROVE} from '@/common/resources/rights.js'
-import {RIGHTS_CR_ASSIGN} from '@/common/resources/rights.js'
-import {RIGHTS_CR_CLOSE} from '@/common/resources/rights'
-import {RIGHTS_CR_MERGE} from '@/common/resources/rights'
+import {
+    ROLE_ADMIN,
+    ROLE_ARCHITECT,
+    ROLE_MANAGER,
+    ROLE_ESTIMATOR
+} from '@/common/resources/roles'
 
-import {RIGHTS_TASK_CREATE} from '@/common/resources/rights'
-import {RIGHTS_TASK_EDIT} from '@/common/resources/rights'
-import {RIGHTS_TASK_DELETE} from '@/common/resources/rights'
-
-import {RIGHTS_SUBTASK_CREATE} from '@/common/resources/rights'
-import {RIGHTS_SUBTASK_EDIT} from '@/common/resources/rights'
-import {RIGHTS_SUBTASK_DELETE} from '@/common/resources/rights'
-
-import {RIGHTS_ESTIMATION_CREATE} from '@/common/resources/rights'
+const managerRights = [
+    RIGHTS_CR_APPROVE,
+    RIGHTS_CR_ASSIGN,
+    RIGHTS_CR_CLOSE,
+    RIGHTS_CR_MERGE,
+    RIGHTS_TASK_CREATE,
+    RIGHTS_TASK_EDIT,
+    RIGHTS_TASK_DELETE,
+    RIGHTS_SUBTASK_CREATE,
+    RIGHTS_SUBTASK_EDIT,
+    RIGHTS_SUBTASK_DELETE
+]
+const estimatorRights = [
+    RIGHTS_ESTIMATION_CREATE
+]
+const architectRights = [
+    RIGHTS_CR_CREATE,
+    RIGHTS_CR_EDIT,
+    RIGHTS_CR_DELETE
+]
 
 export default {
     check(role, right) {
@@ -27,55 +49,17 @@ export default {
             return false
         }
 
-        switch(right) {
-            case RIGHTS_CR_CREATE:
-                return this.changeRequestCreate(role)
-            case RIGHTS_CR_EDIT:
-                return this.changeRequestEdit(role)
-            case RIGHTS_CR_DELETE:
-                return this.changeRequestDelete(role)
-            case RIGHTS_CR_APPROVE:
-                return this.changeRequestApprove(role)
-            case RIGHTS_CR_ASSIGN:
-                return this.changeRequestAssign(role)
-            case RIGHTS_CR_CLOSE:
-                return this.changeRequestClose(role)
-            case RIGHTS_CR_MERGE:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_TASK_CREATE:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_TASK_EDIT:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_TASK_DELETE:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_SUBTASK_CREATE:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_SUBTASK_EDIT:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_SUBTASK_DELETE:
-                return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-            case RIGHTS_ESTIMATION_CREATE:
-                return (role === ROLE_ADMIN || role === ROLE_ESTIMATOR) ? true : false
+        switch(role) {
+            case ROLE_ADMIN:
+                return true
+            case ROLE_MANAGER:
+                return managerRights.indexOf(right) !== -1
+            case ROLE_ARCHITECT:
+                return architectRights.indexOf(right) !== -1
+            case ROLE_ESTIMATOR:
+                return estimatorRights.indexOf(right) !== -1
             default:
                 return false
         }
-    },
-    changeRequestCreate(role) {
-        return (role === ROLE_ADMIN || role === ROLE_ARCHITECT) ? true : false
-    },
-    changeRequestEdit(role) {
-        return (role === ROLE_ADMIN || role === ROLE_ARCHITECT) ? true : false
-    },
-    changeRequestDelete(role) {
-        return (role === ROLE_ADMIN || role === ROLE_ARCHITECT) ? true : false
-    },
-    changeRequestApprove(role) {
-        return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-    },
-    changeRequestAssign(role) {
-        return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
-    },
-    changeRequestClose(role) {
-        return (role === ROLE_ADMIN || role === ROLE_MANAGER) ? true : false
     }
 }
